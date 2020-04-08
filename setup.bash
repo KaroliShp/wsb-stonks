b4ff80ab7640a534f10cf0f80c441d59295d7361
Karolishp <karolis.spukas@gmail.com>
2020-04-08 12:46:02 +0800

    Delete setup.bash

--- setup.bash
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH="$PWD"
-source wsb-stonks/bin/activate
-export FLASK_APP=main.py
-export FLASK_ENV=development
-export MONGO_URL="mongodb+srv:/username:password@secret_MongoDB_Atlas_cluster_location_on_gcp"
