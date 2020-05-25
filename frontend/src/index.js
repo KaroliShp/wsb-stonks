7bca7e6095ace9aa931c62921f154c0d0fbf48cc
rkj <rytis.kazimieras@gmail.com>
2020-05-26 00:03:16 +0800

    Add Google Analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,16 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
 import * as Sentry from '@sentry/browser'
+import ReactGA from 'react-ga'
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: "https://d20d50d40ae544d9ab388546cc53b1ec@o378312.ingest.sentry.io/5201553"
   });
 }
+
+ReactGA.initialize('UA-167581211-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
